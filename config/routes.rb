15d0829821a9e43785070b6dbfea5545bfaf8d84
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-23 22:26:37 +0900

    pets index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'pets#index'
   resources :users, only: [:new, :create,]
-  resources :pets, only: [:index]
+  resources :pets, only: :index
 
 end
