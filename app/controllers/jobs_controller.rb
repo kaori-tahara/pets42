8c0219efa71fc1bbee081555f5a5606432f53eaf
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-24 19:21:56 +0900

    アンダーバー間違い

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,3 +1,49 @@
 class JobsController < ApplicationController
+  class ItemsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_item, only: [:show, :edit, :destroy, :update]
   
+    def index
+      @jobs = Item.includes(:user).order('created_at DESC')
+    end
+  
+    def new
+      @item = Item.new
+    end
+  
+    def create
+      @item = Item.new(item_params)
+      if @item.save
+        redirect_to root_path
+      else
+        render :new
+      end
+    end
+  
+    def update
+      if @item.update(item_params)
+        redirect_to root_path
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      if @item.destroy
+        redirect_to root_path
+      else
+        render :show
+      end
+    end
+  
+    private
+  
+    def item_params
+      params.require(:item).permit(:name, :text, :price, :picture, :category_id, :item_statue_id, :shipping_charge_id, :prefecture_id, :period_until_shipping_id).merge(user_id: current_user.id)
+    end
+  
+    def set_item
+      @item = Item.find(params[:id])
+    end
+  end
 end
