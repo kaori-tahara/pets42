3226baa6de512d575f64d53fc61522b159321265
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-25 18:10:53 +0900

    jobのindex

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,49 +1,51 @@
 class JobsController < ApplicationController
-  class ItemsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
-    before_action :set_item, only: [:show, :edit, :destroy, :update]
+    before_action :set_job, only: [:show, :destroy, :update]
   
     def index
-      @jobs = Item.includes(:user).order('created_at DESC')
+      @jobs = Job.includes(:user).order('created_at DESC')
+    end
+
+    def show
     end
   
-    def new
-      @item = Item.new
+    # def new
+    #   @item = Job.new
+    # end
+  
+    # def create
+    #   @item = Item.new(item_params)
+    #   if @item.save
+    #     redirect_to root_path
+    #   else
+    #     render :new
+    #   end
+    # end
+  
+    # def update
+    #   if @item.update(item_params)
+    #     redirect_to root_path
+    #   else
+    #     render :edit
+    #   end
+    # end
+  
+    # def destroy
+    #   if @item.destroy
+    #     redirect_to root_path
+    #   else
+    #     render :show
+    #   end
+    # end
+  
+    # private
+  
+    def job_params
+      params.require(:job).permit(:title, :text, :dog_picture, :dog_name, :dog_bleed, :dog_age, :working_hours_id, :working_date_id, :prefecture_id, :city, :working_time_id).merge(user_id: current_user.id)
     end
   
-    def create
-      @item = Item.new(item_params)
-      if @item.save
-        redirect_to root_path
-      else
-        render :new
-      end
+    def set_job
+      @job = job.find(params[:id])
     end
   
-    def update
-      if @item.update(item_params)
-        redirect_to root_path
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      if @item.destroy
-        redirect_to root_path
-      else
-        render :show
-      end
-    end
-  
-    private
-  
-    def item_params
-      params.require(:item).permit(:name, :text, :price, :picture, :category_id, :item_statue_id, :shipping_charge_id, :prefecture_id, :period_until_shipping_id).merge(user_id: current_user.id)
-    end
-  
-    def set_item
-      @item = Item.find(params[:id])
-    end
-  end
 end
