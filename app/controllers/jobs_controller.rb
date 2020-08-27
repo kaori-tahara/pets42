8a324075fb3e9376f4d58b58fd1bff2f8becc829
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-27 11:50:49 +0900

    ジョブのshow,edit作成

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,13 +1,11 @@
 class JobsController < ApplicationController
-    # before_action :authenticate_user!, except: [:index, :show]
-    before_action :set_job, only: [:show, :destroy, :update]
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_job, only: [:show, :edit, :destroy, :update]
   
     def index
       @jobs = Job.includes(:user).order('created_at DESC')
     end
 
-    def show
-    end
   
     def new
       @job = Job.new
@@ -22,21 +20,21 @@ class JobsController < ApplicationController
       end
     end
   
-    # def update
-    #   if @item.update(item_params)
-    #     redirect_to root_path
-    #   else
-    #     render :edit
-    #   end
-    # end
+    def update
+      if @job.update(job_params)
+        redirect_to jobs_path
+      else
+        render :edit
+      end
+    end
   
-    # def destroy
-    #   if @item.destroy
-    #     redirect_to root_path
-    #   else
-    #     render :show
-    #   end
-    # end
+    def destroy
+      if @job.destroy
+        redirect_to jobs_path
+      else
+        render :show
+      end
+    end
   
     private
   
