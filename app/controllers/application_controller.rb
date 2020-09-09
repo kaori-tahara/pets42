cb21958517b88ad5d646607026eed29fbaa2be1b
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-09-09 12:40:36 +0900

    prefectureの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :first_name, :family_name, :first_kana, :family_kana, :birth, :zip, :prefecture, :city, :phone])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :first_name, :family_name, :first_kana, :family_kana, :birth, :zip, :prefecture_id, :city, :phone])
     
   end
 end
