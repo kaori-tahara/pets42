17933e585f622dd36525a6a5a620f4d04283e0f5
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-09-09 15:18:52 +0900

    migration変更

--- db/migrate/20200828071028_create_addresses.rb
+++ db/migrate/20200828071028_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.string :zip,null: false
-      t.string :prefecture_id,null: false
+      t.integer :prefecture_id,null: false
       t.string :city,null: false
       t.string :phone,null: false
       t.references :user
